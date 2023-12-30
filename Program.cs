182f811e34be7fb9db0ba46e8a420d0753bf96e1
Ildar Kashapov <kimestnine@gmail.com>
2023-12-30 22:16:44 +0300

    Task about % 7&23

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%7==0 && a%23==0)
+{
+    Console.WriteLine("Число делится на 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число не делится на 7 и 23 ");
+}
